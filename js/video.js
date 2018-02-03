1dac1a63e7d33dbadfa8c5f92e987e06a2746cc1
aqfan <angelf@seas.upenn.edu>
2018-02-03 16:20:56 -0500

    catch promise

--- js/video.js
+++ js/video.js
@@ -9,8 +9,8 @@ function onSubmit(){
   video_player = document.getElementById("video");
   input_text = $("#translate-text").val();
   video_list = [];
+  video_index = 0;
   if (!input_text || input_text.length !== 0) {
-    console.log(input_text);
     parseText();
   }
 }
@@ -23,34 +23,41 @@ function parseText() {
   var arr = input_text.split(" ");
   for (var i = 0; i < arr.length; i++) {
     var str = "https://www.handspeak.com/word/"+arr[i].charAt(0)+"/"+arr[i]+".mp4";
-    console.log(str);
     video_list.push(str);
   }
+  console.log(video_list);
   video_player.src = video_list[video_index];
-  startPlayback();
+  var playPromise = video_player.play();
+
+  // In browsers that don’t yet support this functionality,
+  // playPromise won’t be defined.
+  if (playPromise !== undefined) {
+    playPromise.then(function() {
+      console.log("auto playback started!");
+    }).catch(function(error) {
+      console.error('Failed to start video, retrying');
+      parseText();
+    });
+  }
 }
-
-function startPlayback() {
-  console.log("startplayback");
-    video_player.play();
-}
-
-startPlayback.then(function () {
-  console.log(video_player.currentTime, video_player.duration);
-}).catch(function(error) {
-        console.error('Failed to start video, retrying');
-        var playButton = document.querySelector('#submit');
-        playButton.addEventListener('click', startPlayback);
-        playButton.hidden = false;
-});
-
 
 function onVideoEnded() {
   console.log("video ended");
   if(video_index < video_list.length - 1) {
     video_index++;
     video_player.src = video_list[video_index];
-    startPlayback();
+    var playPromise = video_player.play();
+
+    // In browsers that don’t yet support this functionality,
+    // playPromise won’t be defined.
+    if (playPromise !== undefined) {
+      playPromise.then(function() {
+        console.log("auto playback started!");
+      }).catch(function(error) {
+        console.error('Failed to start video, retrying');
+        parseText();
+      });
+    }
   } else {
     video_player.pause();
   }
