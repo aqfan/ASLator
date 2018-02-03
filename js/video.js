40cd58e804b7e5d85b03f86526a4fec887c032e0
aqfan <angelf@seas.upenn.edu>
2018-02-03 16:06:21 -0500

    catch promise

--- js/video.js
+++ js/video.js
@@ -27,15 +27,30 @@ function parseText() {
     video_list.push(str);
   }
   video_player.src = video_list[video_index];
-  video_player.play();
+  startPlayback();
 }
+
+function startPlayback() {
+  console.log("startplayback");
+    video_player.play();
+}
+
+startPlayback.then(function () {
+  console.log(video_player.currentTime, video_player.duration);
+}).catch(function(error) {
+        console.error('Failed to start video, retrying');
+        var playButton = document.querySelector('#submit');
+        playButton.addEventListener('click', startPlayback);
+        playButton.hidden = false;
+});
+
 
 function onVideoEnded() {
   console.log("video ended");
   if(video_index < video_list.length - 1) {
     video_index++;
     video_player.src = video_list[video_index];
-    video_player.play();
+    startPlayback();
   } else {
     video_player.pause();
   }
