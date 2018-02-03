b89ee6ec18ef78c8f089cfdac16b4adb1b843dff
cldon <cldon@seas.upenn.edu>
2018-02-03 14:36:27 -0500

    updated translate
    input: sentence
    output: array of words, in order

--- translate.py
+++ translate.py
@@ -1,24 +1,34 @@
 import spacy
 nlp = spacy.load('en')
-parsed_text = nlp(u"I thought it's was the complete set")
-print(parsed_text)
-subject = ''
-indirect_object = ''
-direct_object = ''
 
-#get token dependencies
-for text in parsed_text:
-    # #subject would be
-    # if text.dep_ == "nsubj":
-    #     subject = text.orth_
-    # #iobj for indirect object
-    # if text.dep_ == "iobj":
-    #     indirect_object = text.orth_
-    # #dobj for direct object
-    # if text.dep_ == "dobj":
-    #     direct_object = text.orth_
-    print(text.tag_)
+WORD_ORDER = ["SUB", "IO", "DO", "VERB"]
 
-# print(subject)
-# print(direct_object)
-# print(indirect_object)
+def translate(sentence):
+	parsed_text = nlp(unicode(sentence))
+	# print(parsed_text)
+	subject = ''
+	indirect_object = ''
+	direct_object = ''
+
+	output = []
+
+	#get token dependencies
+	for text in parsed_text:
+		text = text.lemma_
+		# print(text)
+		print("Tag: " + text.tag_)
+		print("Dep: " + text.dep_)
+		if text.dep_ == 'nsubj':
+			output.append(text)
+		elif text.tag_[:2] == 'VB':
+			output.append(text)
+		elif text.dep_ == "dobj":
+			output.append(text)
+
+	# for el in WORD_ORDER:
+	# 	if 
+	print(output)
+	return output
+	# print(subject)
+	# print(direct_object)
+	# print(indirect_object)
